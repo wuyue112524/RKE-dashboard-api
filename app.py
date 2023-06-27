b69560c6c21514ce1405ace2ce6500a559c9ae2d
wuyue112524 <you@example.com>
2023-06-27 10:16:48 +0100

    clean out script comments

--- app.py
+++ app.py
@@ -12,42 +12,14 @@ with open(config_path) as credential:
 app = Flask(__name__)
 CORS(app)
 
-# Replace these with your actual data
+# connect to database
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{config["conn"]["user"]}:{config["conn"]["password"]}@{config["conn"]["host"]}/{config["conn"]["database"]}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
 
-
-# @app.route('/api/countries', methods=['GET'])
-# def get_countries():
-#     countries = Country.query.all()
-#     output = []
-    
-#     for country in countries:
-#         country_data = {'country_id': country.country_id, 'country_name': country.country_name, 'country_iso_a2': country.country_iso_a2}
-#         output.append(country_data)
-#     return jsonify(output)
-
-# @app.route('/api/regulators', methods=['GET'])
-# def get_regulators():
-#     regulators = Regulator.query.all()
-#     output = []
-#     for regulator in regulators:
-#         regulator_data = {'response_id': regulator.response_id, 'organization_name': regulator.organization_name, 'respondent_name': regulator.respondent_name,
-#                           'country_code':regulator.country_code, 'regulator_type':regulator.regulator_type,'a1_priority_change':regulator.a1_priority_change,
-#                           'region_wb':regulator.region_wb, 'a3_consumer_protection':regulator.a3_consumer_protection}
-#         output.append(regulator_data)
-#     return jsonify(output)
-
-# @app.route('/api/countries/<id>', methods=['GET'])
-# def get_country(id):
-#     country = Country.query.get(id)
-#     if country is None:
-#         return {"error": "not found"}
-#     return {'country_id': country.country_id, 'country_name': country.country_name, 'country_iso_a2': country.country_iso_a2}
-
+# create api endpoint
 @app.route('/api/rke', methods = ['GET'])
 def get_rke():
     rke = RKE.query.all()
